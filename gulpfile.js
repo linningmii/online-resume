325ced643e2445ed09d60e0d13b04858b25a45e6
linzhongming <lin.zh.ming.27@gmail.com>
2017-02-07 22:57:33 +0800

    Configure gulp + browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var
-  gulp = require('gulp');
+  gulp = require('gulp'),
+  browserify = require('browserify'),
+  source = require('vinyl-source-stream'),
+  babelify = require('babelify');
 
-gulp.task('default', function () {
-
+gulp.task('build', function () {
+  browserify('./src/index.js')
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist'))
 });
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*.js', ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
