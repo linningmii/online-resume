fd50d30e09dac65b61bf2eb64baeeaa530c201a6
linzhongming <lin.zh.ming.27@gmail.com>
2017-02-09 22:45:56 +0800

    now support auto reload, add usage in README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+#### run
+
+```
+yarn install
+gulp
+```
+
+open browser [http://localhost:9999](http://localhost:9999)
+
--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,49 @@ var
   browserify = require('browserify'),
   source = require('vinyl-source-stream'),
   babelify = require('babelify'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  stringify = require('stringify'),
+  connect = require('gulp-connect');
 
-gulp.task('build', function () {
-  browserify('./src/index.js')
-    .transform(babelify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./dist'));
-
+gulp.task('html', function () {
   gulp.src('./src/index.html')
     .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
 });
 
 gulp.task('style', function () {
   gulp.src('src/style/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  browserify('./src/index.js')
+    .transform(stringify, {
+      appliesTo: {
+        includeExtensions: ['.html']
+      }
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./src/**/*.js', ['build']);
-  gulp.watch('./src/**/*.html', ['build']);
+  gulp.watch('./src/**/*.js', ['js']);
+  gulp.watch('./src/**/*.html', ['html']);
   gulp.watch('./src/**/*.scss', ['style']);
 });
 
-gulp.task('default', ['build', 'style', 'watch']);
+gulp.task('server', function () {
+  connect.server({
+    livereload: true,
+    port: 9999,
+    root: './dist'
+  });
+});
+
+gulp.task('default', ['html', 'style', 'js', 'watch', 'server']);
